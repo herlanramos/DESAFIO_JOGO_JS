25edf81d43ab2b6a49308dedda4ee16c66e26286
herlanramos <hrlramos@gmail.com>
2021-12-01 14:53:23 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -8,4 +14,12 @@ function criarBG() {
 
 }
 
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "black";
+        context.fillRect(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 criarBG();
+criarCobrinha();
