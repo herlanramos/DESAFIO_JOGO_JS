257bdbfe51d2a019e86c77c3c4712a4c8c5644fa
herlanramos <hrlramos@gmail.com>
2021-12-01 19:06:13 -0300

    cria da comida para a cobrinha

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +26,11 @@ function criarCobrinha(){
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -46,6 +54,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
